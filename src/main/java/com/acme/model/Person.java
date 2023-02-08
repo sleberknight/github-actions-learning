8def8ccbe37940cb1550a247a533af22f1ae9b9c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-02-08 22:20:48 +0000

    Add a Lombok With to Person

--- src/main/java/com/acme/model/Person.java
+++ src/main/java/com/acme/model/Person.java
@@ -3,12 +3,16 @@ package com.acme.model;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
+import lombok.With;
 
 import java.time.LocalDate;
 
 @Value
 @Builder
 public class Person {
+
+    @With
+    Long id;
 
     @NonNull
     String firstName;
